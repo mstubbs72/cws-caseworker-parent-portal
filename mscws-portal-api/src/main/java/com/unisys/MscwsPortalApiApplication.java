3f0a0e9675289a5f7cb57fbbc21c7f8ee9caab34
Kirtish Kulkarni <cooldudekirth@gmail.com>
2016-09-28 19:10:22 +0530

    Revert "Committing Test Cases for Basic Search"
    This reverts commit 3f651b069057971e22e061009c2b5dee42e5d849.

---
+++ mscws-portal-api/src/main/java/com/unisys/MscwsPortalApiApplication.java
@@ -0,0 +1,12 @@
+package com.unisys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscwsPortalApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscwsPortalApiApplication.class, args);
+	}
+}
